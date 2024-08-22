6da0283b6a81a147e5ea358596bffcb25fe4f64a
mounikakurukuntla <96129389+mounikakurukuntla@users.noreply.github.com>
2024-08-22 14:56:45 +0530

    Sql queries (#2)
    * created initial solution (#1)
    
    * Updated DataBase
    
    ---------
    
    Co-authored-by: raja1702 <100635731+raja1702@users.noreply.github.com>

---
+++ ComputerStorageSolution/Models/UserDBModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace ComputerStorageSolutions.Models
+{
+    public class UserDbModel
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(255)]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(255)]
+        public string Email { get; set; } = string.Empty;
+
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; }
+
+        // Navigation property
+        public virtual RoleDbModel Role { get; set; }
+    }
+
+}
