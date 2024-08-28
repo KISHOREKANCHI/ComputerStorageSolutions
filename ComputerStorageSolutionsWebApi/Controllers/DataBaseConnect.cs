68238ac2fd2945efe6d817f7b0c80135caa6129f
raja1702 <100635731+raja1702@users.noreply.github.com>
2024-08-28 12:23:11 +0530

    Logging branch (#6)
    * added orders controller
    
    * implemented loggin and did some changes to validation

--- ComputerStorageSolutionsWebApi/Controllers/DataBaseConnect.cs
+++ ComputerStorageSolutionsWebApi/Controllers/DataBaseConnect.cs
@@ -6,6 +6,7 @@ namespace ComputerStorageSolutions.Controllers
     public class DataBaseConnect : DbContext
     {
         public DataBaseConnect(DbContextOptions<DataBaseConnect> options) : base(options) { }
+        
         public DbSet<UserModel> Users { get; set; }
 
         public DbSet<CategoryModel> Categories { get; set; }
@@ -13,9 +14,11 @@ namespace ComputerStorageSolutions.Controllers
         public DbSet<ProductsModel> Products { get; set; }
 
         public DbSet<RoleModel> Roles { get; set; }
+        
         public DbSet<OrdersModel> Orders { get; set; }
+        
         public DbSet<OrderDetailsModel> OrderDetails { get; set; }
-
+        
         public DbSet<InvoicesModel> Invoices { get; set; }
 
         public DbSet<CartModel> Cart { get; set; }
