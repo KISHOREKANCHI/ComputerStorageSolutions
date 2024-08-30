8c2ec3a35e8005226b23843f437e7f6bbb617516
KISHOREKANCHI <kanchikrishnasaikishore91@gmail.com>
2024-08-30 11:54:44 +0530

    modified signup page

--- ComputerStorageSolutionsWebsite/src/app/app-routing.module.ts
+++ ComputerStorageSolutionsWebsite/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginpageComponent } from './loginpage/loginpage.component';
 import { ProductpageComponent } from './productpage/productpage.component';
+import { SignuppageComponent } from './signuppage/signuppage.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'/login',pathMatch:'full'},
   {path:'login',component:LoginpageComponent},
-  {path:'products',component:ProductpageComponent}
+  {path:'products',component:ProductpageComponent},
+  {path:'signup',component:SignuppageComponent}
 ];
 
 @NgModule({
--- ComputerStorageSolutionsWebsite/src/app/loginpage/loginpage.component.ts
+++ ComputerStorageSolutionsWebsite/src/app/loginpage/loginpage.component.ts
@@ -38,4 +38,8 @@ export class LoginpageComponent {
       }
     });
   }
+
+  SignUpRedirect(){
+    this.router.navigate(['signup'])
+  }
 }
