b3d39616ed62058a3be90cdd121ef69547456ba7
KISHOREKANCHI <kanchikrishnasaikishore91@gmail.com>
2024-09-03 23:27:48 +0530

    complete working status of AddProduct

--- ComputerStorageSolutionsWebsite/src/app/add-productpage/add-productpage.component.ts
+++ ComputerStorageSolutionsWebsite/src/app/add-productpage/add-productpage.component.ts
@@ -14,14 +14,16 @@ export class AddProductpageComponent {
   product = {
     productName: '',
     description: '',
-    price: 0,
+    price: '',
     categoryId: '',
-    stockQuantity: 0,
+    stockQuantity: '',
     status: '',
   };
   popupVisible: boolean = false;
   popupText: string = '';
   selectedFile: File | null = null;
+  uploadedImage: string | ArrayBuffer | null = null;
+  errorMessage: string = '';
 
   // Property to toggle the upload option visibility
   showUploadOption = false;
@@ -36,6 +38,14 @@ export class AddProductpageComponent {
   }
 
   addProduct() {
+    this.errorMessage = ''
+
+    if (!this.product.productName || !this.product.description || 
+      this.product.price == null || !this.product.categoryId || 
+      this.product.stockQuantity == null || !this.product.status) {
+      this.errorMessage = 'All fields are required!';
+      return;
+    }
     if (this.selectedFile) {
       const formData = new FormData();
       formData.append('image', this.selectedFile);
@@ -46,26 +56,14 @@ export class AddProductpageComponent {
       formData.append('stockQuantity', this.product.stockQuantity.toString()); // Ensure stockQuantity is a string
       formData.append('status', this.product.status);
 
-      console.log('Form Data Entries:');
-    console.log('Image:', formData.get('image'));
-    console.log('Product Name:', formData.get('productName'));
-    console.log('Description:', formData.get('description'));
-    console.log('Price:', formData.get('price'));
-    console.log('Category ID:', formData.get('categoryId'));
-    console.log('Stock Quantity:', formData.get('stockQuantity'));
-    console.log('Status:', formData.get('status'));
       this.apiService.AddProduct(formData).subscribe({
         next: response => {
           this.showPopup(`${this.product.productName} added successfully`);
-          // this.resetForm(); // Optionally reset the form after successful addition
+          this.resetForm(); // Optionally reset the form after successful addition
         },
         error: err => {
-          console.log(err);
           this.showPopup('Error adding product');
         },
-        complete: () => {
-          console.log('Product upload complete');
-        }
       });
     } else {
       this.showPopup('Please select an image');
@@ -76,7 +74,14 @@ export class AddProductpageComponent {
   onFileChange(event: any,files:any) {
     if (event.target.files && event.target.files.length > 0) {
       this.selectedFile = <File>files[0]; // Set the selected file
-      console.log("Selected file:", this.selectedFile);
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        // Check if e.target is not null
+        if (e.target && e.target.result) {
+          this.uploadedImage = e.target.result; // Set uploaded image to display
+        }
+      };
+      reader.readAsDataURL(this.selectedFile);
     }
   }
 
@@ -85,12 +90,15 @@ export class AddProductpageComponent {
     this.product = {
       productName: '',
       description: '',
-      price:0,
+      price: '',
       categoryId: '',
-      stockQuantity: 0,
-      status: 'available'
+      stockQuantity: '',
+      status: ''
     };
     this.selectedFile = null; // Reset selected file
+    this.uploadedImage = null; // Clear the uploaded image
     this.showUploadOption = false; // Hide upload option
+    this.errorMessage = ''; // Clear any error message
   }
+  
 }
